e28d37ae75cb84ef0f2f1f4fd30d93aa5d7f7b91
Megan H <meganhonig@gmail.com>
2017-10-02 14:37:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,3 +17,18 @@ function divide(a,b) {
   var div = a/b;
   return div;
 }
+
+function inc(n) {
+  n++
+  return n;
+}
+
+function dec(n) {
+  n--;
+  return n;
+}
+
+function makeInt(n) {
+return parseInt(n);
+}
+
