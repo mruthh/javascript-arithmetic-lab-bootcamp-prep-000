33c0e041c104bd0ed298493bcd30087be816f832
Megan H <meganhonig@gmail.com>
2017-10-02 14:35:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,3 +7,13 @@ function subtract(a,b) {
   var diff = a - b;
   return diff;
 }
+
+function multiply (a,b) {
+  var prod = a * b;
+  return prod;
+}
+
+function divide(a,b) {
+  var div = a/b;
+  return div;
+}
